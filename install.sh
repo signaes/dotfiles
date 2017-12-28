616b44802bc79c9a6afb475b11d762bc0464f48b
Thiago Oliveira <thiago@signaes.com>
2017-12-27 23:23:38 -0200

    Fix conditionals. Add git-completion.bash download

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-[[ $(which brew)  == "" ]] && echo 'installing homebrew' \
+[[ $(which brew)  == "" ]] && echo 'installing homebrew' && \
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-[[ $(ls ~/.vim/autoload/plug.vim 2> /dev/null)  == "" ]] && echo 'installing vim-plug' \
+[[ $(ls ~/.vim/autoload/plug.vim 2> /dev/null)  == "" ]] && echo 'installing vim-plug' && \
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-[[ $(which fzf) == "" ]] && echo 'installing fzf' \
+[[ $(which fzf) == "" ]] && echo 'installing fzf' && \
   brew install fzf
 
-[[ $(which ag) == "" ]] && echo 'installing ag' \
+[[ $(which ag) == "" ]] && echo 'installing ag' && \
   brew install the_silver_searcher
+
+[[ $(ls ~/.git-completion.bash 2> /dev/null) == "" ]] && echo 'installing git-completion' && \
+  curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
 echo 'symlinking vimrc, tmux.conf and bash_profile'
 ln -sf "$(pwd)/vimrc" ~/.vimrc
