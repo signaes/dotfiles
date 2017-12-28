1e18fb0854b5195a3ef7d536c62ee45716a6d0cb
Thiago Oliveira <thiago@signaes.com>
2017-12-27 23:12:10 -0200

    Add conditionals

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
+[[ $(which brew)  == "" ]] && echo 'installing homebrew' \
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+[[ $(ls ~/.vim/autoload/plug.vim 2> /dev/null)  == "" ]] && echo 'installing vim-plug' \
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+[[ $(which fzf) == "" ]] && echo 'installing fzf' \
+  brew install fzf
+
+[[ $(which ag) == "" ]] && echo 'installing ag' \
+  brew install the_silver_searcher
+
+echo 'symlinking vimrc, tmux.conf and bash_profile'
 ln -sf "$(pwd)/vimrc" ~/.vimrc
 ln -sf "$(pwd)/tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/bash_profile" ~/.bash_profile
-mkdir ~/.vim/backup ~/.vim/swap
+
+echo 'making ~/.vim/backup and ~/.vim/swap directories'
+mkdir ~/.vim/backup ~/.vim/swap 2> /dev/null
